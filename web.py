f7760b9034a682322c5b47865a7bfa53a2a0f719
EC2 Default User <ec2-user@ip-172-31-28-3.ap-southeast-1.compute.internal>
2020-04-24 13:14:34 +0000

    update jenkinsfile 3

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, JZ's Dockerized demo!! Github-->JenkinPipeline-->DockerHub"
+    return "Hello, JZ's Dockerized demo!! Github-->JenkinPipeline-->DockerHub.."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
