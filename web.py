bdcbaec8db1ab5062be57ea285188dc4157b4020
EC2 Default User <ec2-user@ip-172-31-28-3.ap-southeast-1.compute.internal>
2020-04-24 12:49:56 +0000

    Mod web.py output

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, JZ Dockerized demo!!"
+    return "Hello, JZ Dockerized demo!! Github-->Jenkin-->DockerHub"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
